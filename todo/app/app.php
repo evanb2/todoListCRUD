18d4a47d7696feca0a97411697eb2791ea1b5bfe
Reid Baldwin & Evan Butler <student@epicodus.com>
2015-03-18 09:41:13 -0700

    add initial update file code

--- todo/app/app.php
+++ todo/app/app.php
@@ -11,6 +11,8 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.twig', array('categories' => Category::getAll()));
@@ -20,6 +22,19 @@
         $category = Category::find($id);
         return $app['twig']->render('category.twig', array('category' => $category, 'tasks' => $category->getTasks()));
     });
+
+    $app->get("/categories/{id}/edit", function($id) use ($app) {
+        $category = Category::find($id);
+        return $app['twig']->render('category_edit.html.twig', array('category' => $category);
+    });
+
+    $app->patch("/categories/{id}", function($id) use ($app) {
+        $name = $_POST['name'];
+        $category = Category::find($id);
+        $category->update($name);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
+    });
+
 
     $app->get("/tasks", function() use ($app) {
         return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
